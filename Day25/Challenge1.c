db82b4ac4da71f3a16a793d91e0891771217c1b0
Karthiga-G <156902767+Karthiga-G@users.noreply.github.com>
2024-02-26 05:18:00 +0530

    Challenge1.c

---
+++ Day25/Challenge1.c
@@ -0,0 +1,21 @@
+//Challenge
+
+/*
+Please fill in the lines of code to implement the logic present in the flowchart
+*/
+
+//Solution
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    while(x<=y){
+        printf("%d ", x);
+        x+=1;
+    }
+
+}
