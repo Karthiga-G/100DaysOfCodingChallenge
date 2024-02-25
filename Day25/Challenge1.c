b8acd28aafe89d97617a2adbd6b4132ec1c84962
Karthiga-G <156902767+Karthiga-G@users.noreply.github.com>
2024-02-26 05:22:58 +0530

    Challenge1.c

--- Day25/Challenge1.c
+++ Day25/Challenge1.c
@@ -1,7 +1,7 @@
 //Challenge
 
 /*
-Please fill in the lines of code to implement the logic present in the flowchart
+Please fill in the lines of code to implement the logic present in the flowchart[https://github.com/Karthiga-G/30DaysOfCodingChallenge/blob/main/Day25/C1_flowchart.png]
 */
 
 //Solution
