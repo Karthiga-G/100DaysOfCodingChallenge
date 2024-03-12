626b7b24315e1b0f8c52284290010fb57262a361
Karthiga <156902767+Karthiga-G@users.noreply.github.com>
2024-03-12 19:05:09 +0530

    Challenge2.java

---
+++ Day35/Challenge2.java
@@ -0,0 +1,47 @@
+//Challenge - Sum of Power of Digits
+
+/*
+The program must accept an integer N as the input. The program must print the sum of each digit
+raised to the power of the number of digits in N as the output.
+Boundary Condition(s):
+1 <= N <= 10^8
+Input Format:
+The first line contains N.
+Output Format:
+The first line contains the sum of each digit raised to the power of the number of digits in N.
+Example Input/Output 1:
+Input:
+2356
+Output:
+2018
+Explanation:
+The number of digits in 2356 is 4.
+(24 + 34 + 54 + 64
+) = (16 + 81 + 625 + 1296) = 2018
+Hence the output is 2018
+Example Input/Output 2:
+Input:
+78100
+Output:
+49576
+*/
+
+//Solution
+
+import java.util.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+		double sum=0.0;
+		String s = Integer.toString(n);
+		for(int i=0; i<s.length(); i++){
+		    double digit=Integer.parseInt(String.valueOf(s.charAt(i)));
+		    double power= Math.pow(digit, s.length());
+		    sum+=power;
+		}
+		System.out.println((int)sum);
+	}
+}
